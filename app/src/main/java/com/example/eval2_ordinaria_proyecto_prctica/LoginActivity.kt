c9f5f75b736fa8cc90808e3b63b5ffb33b794922
Julio <julsie157@alu.centroafuera.es>
2024-02-10 03:42:24 +0100

    lololo

--- app/src/main/java/com/example/eval2_ordinaria_proyecto_prctica/LoginActivity.kt
+++ app/src/main/java/com/example/eval2_ordinaria_proyecto_prctica/LoginActivity.kt
@@ -1,96 +1,95 @@
 package com.example.eval2_ordinaria_proyecto_prctica
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.google.firebase.firestore.FirebaseFirestore
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var editTextUser: EditText
+    private lateinit var auth: FirebaseAuth
+    private lateinit var editTextUsername: EditText
     private lateinit var editTextPassword: EditText
     private lateinit var buttonLogin: Button
+    private lateinit var textViewRegister: TextView
 
-
-    private lateinit var mAuth: FirebaseAuth
-
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_login) // Asegúrate de tener el nombre correcto de tu layout aquí
 
+        // Inicializa Firebase Auth
+        auth = FirebaseAuth.getInstance()
 
-        mAuth = FirebaseAuth.getInstance()
-
-
-        editTextUser = findViewById(R.id.editTextUserLogin)
+        // Referencias a las vistas
+        editTextUsername = findViewById(R.id.editTextUserLogin)
         editTextPassword = findViewById(R.id.editTextPassLogin)
         buttonLogin = findViewById(R.id.buttonLogin)
+        textViewRegister = findViewById(R.id.textViewRegister)
 
         buttonLogin.setOnClickListener {
-            loginUser()
+            val username = editTextUsername.text.toString().trim()
+            val password = editTextPassword.text.toString().trim()
+            signInWithUsername(username, password)
         }
 
-       val textViewRegister = findViewById<TextView>(R.id.textViewRegister)
         textViewRegister.setOnClickListener {
+            // Navegar a RegistroActivity
             val intent = Intent(this, RegistroActivity::class.java)
             startActivity(intent)
         }
     }
 
-    private fun loginUser() {
-        val username = editTextUser.text.toString().trim()
-        val password = editTextPassword.text.toString().trim()
-
-        if (username.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Por favor, ingrese su usuario y contraseña.", Toast.LENGTH_SHORT).show()
+    private fun signInWithUsername(username: String, password: String) {
+        if (username.isBlank() || password.isBlank()) {
+            Toast.makeText(this, "Usuario y contraseña no pueden estar vacíos", Toast.LENGTH_SHORT).show()
             return
         }
 
-        getEmailForUsername(username) { email ->
-            email?.let {
-                // Si el email fue encontrado, intenta iniciar sesión con Firebase Auth
-                mAuth.signInWithEmailAndPassword(it, password).addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        showToast("Inicio de sesión exitoso!")
-                    } else {
-                        Toast.makeText(this, "Autenticación fallida: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            } ?: run {
-                // Si el email es null, muestra un mensaje de error
-                Toast.makeText(this, "Nombre de usuario no encontrado", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
+        Log.d("LoginActivity", "Buscando usuario: $username")
 
-    private fun getEmailForUsername(username: String, completion: (String?) -> Unit) {
-        val db = FirebaseFirestore.getInstance()
-        db.collection("users")
+        FirebaseFirestore.getInstance().collection("users")
             .whereEqualTo("username", username)
-            .limit(1)
             .get()
             .addOnSuccessListener { documents ->
                 if (documents.isEmpty) {
-                    completion(null) // No se encontró el usuario
+                    Log.d("LoginActivity", "Usuario no encontrado en Firestore.")
+                    Toast.makeText(this, "Usuario no encontrado.", Toast.LENGTH_SHORT).show()
                 } else {
-                    val email = documents.firstOrNull()?.getString("email")
-                    completion(email)
+                    val email = documents.first().getString("email") ?: ""
+                    Log.d("LoginActivity", "Correo electrónico encontrado: $email")
+                    signInWithEmail(email, password)
                 }
             }
-            .addOnFailureListener {
-                completion(null)
+            .addOnFailureListener { exception ->
+                Log.d("LoginActivity", "Error al buscar el usuario: ${exception.message}")
+                Toast.makeText(this, "Error al buscar el usuario.", Toast.LENGTH_SHORT).show()
             }
     }
 
 
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    private fun signInWithEmail(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Inicio de sesión exitoso, actualizar UI
+                    val intent = Intent(this, PrincipalActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    when (task.exception) {
+                        is FirebaseAuthInvalidUserException -> Toast.makeText(baseContext, "Usuario no existe.", Toast.LENGTH_SHORT).show()
+                        is FirebaseAuthInvalidCredentialsException -> Toast.makeText(baseContext, "Contraseña incorrecta.", Toast.LENGTH_SHORT).show()
+                        else -> Toast.makeText(baseContext, "Error de autenticación.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
     }
 }
--- app/src/main/java/com/example/eval2_ordinaria_proyecto_prctica/RegistroActivity.kt
+++ app/src/main/java/com/example/eval2_ordinaria_proyecto_prctica/RegistroActivity.kt
@@ -1,120 +1,72 @@
 package com.example.eval2_ordinaria_proyecto_prctica
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegistroActivity : AppCompatActivity() {
 
-    private lateinit var editTextUsername: EditText
+    private lateinit var auth: FirebaseAuth
     private lateinit var editTextEmail: EditText
     private lateinit var editTextPassword: EditText
+    private lateinit var editTextUser: EditText
     private lateinit var buttonConfirmar: Button
 
-    // Instancia de FirebaseAuth
-    private lateinit var mAuth: FirebaseAuth
-
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
 
-        // Inicializa Firebase Auth
-        mAuth = FirebaseAuth.getInstance()
+        // Inicializar Firebase Auth
+        auth = Firebase.auth
 
-        // Inicializa vistas
-        editTextUsername = findViewById(R.id.editTextUserRegister)
+        // Referencias a las vistas
         editTextEmail = findViewById(R.id.editTextEmailRegister)
         editTextPassword = findViewById(R.id.editTextPasswordRegister)
+        editTextUser = findViewById(R.id.editTextUserRegister)
         buttonConfirmar = findViewById(R.id.buttonConfirmar)
 
         buttonConfirmar.setOnClickListener {
-            val email = editTextEmail.text.toString().trim()
-            val password = editTextPassword.text.toString().trim()
-            val username = editTextUsername.text.toString().trim()
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+            val username = editTextUser.text.toString()
 
-            if (validateForm(email, username, password)) {
-                registerUser(email, password, username)
+            if (validateForm(email, password, username)) {
+                registerUser(email, password)
             }
         }
-
     }
 
-    private fun validateForm(email: String, username: String, password: String): Boolean {
-        // Validación del correo electrónico
-        if (email.isEmpty()) {
-            Toast.makeText(this, "El correo electrónico es obligatorio.", Toast.LENGTH_SHORT).show()
+    private fun validateForm(email: String, password: String, username: String): Boolean {
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Ingrese un correo electrónico válido.", Toast.LENGTH_SHORT).show()
             return false
         }
 
-        // Validación del nombre de usuario
-        if (username.isEmpty()) {
-            Toast.makeText(this, "El nombre de usuario es obligatorio.", Toast.LENGTH_SHORT).show()
+        if (password.length < 6 || !password.any { it.isDigit() } || !password.any { it.isUpperCase() }) {
+            Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres, una mayúscula y un número.", Toast.LENGTH_SHORT).show()
             return false
         }
-        if (username.length < 4) {
-            Toast.makeText(this, "El nombre de usuario debe tener al menos 4 caracteres.", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        if (!username.matches("^[A-Za-z]+$".toRegex())) {
-            Toast.makeText(this, "El nombre de usuario solo puede contener letras.", Toast.LENGTH_SHORT).show()
-            return false
-        }
-
-        // Validaciones de la contraseña
-        if (password.isEmpty()) {
-            Toast.makeText(this, "La contraseña es obligatoria.", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        if (password.length < 6) {
-            Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres.", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        if (!password.matches(".*[A-Z].*".toRegex())) {
-            Toast.makeText(this, "La contraseña debe contener al menos una letra mayúscula.", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        if (!password.matches(".*[0-9].*".toRegex())) {
-            Toast.makeText(this, "La contraseña debe contener al menos un número.", Toast.LENGTH_SHORT).show()
-            return false
-        }
-
-        // Si todas las validaciones son exitosas
         return true
     }
 
-
-
-    private fun registerUser(email: String, password: String, username: String) {
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
-            if (task.isSuccessful) {
-                val userId = mAuth.currentUser?.uid // Obtiene el ID del usuario recién creado
-                val userMap = hashMapOf(
-                    "username" to username,
-                    "email" to email
-                )
-                userId?.let {
-                    FirebaseFirestore.getInstance().collection("users").document(it)
-                        .set(userMap)
-                        .addOnSuccessListener {
-                            Toast.makeText(this, "Registro exitoso.", Toast.LENGTH_SHORT).show()
-                            // Aquí puedes redirigir al usuario a la pantalla principal o realizar otras acciones necesarias
-                        }
-                        .addOnFailureListener { e ->
-                            Toast.makeText(this, "Error al guardar el usuario: ${e.message}", Toast.LENGTH_SHORT).show()
-                        }
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Registro exitoso, navegar a LoginActivity
+                    Toast.makeText(this, "Registro exitoso.", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                } else {
+                    // Si el registro falla, muestra un mensaje al usuario.
+                    Toast.makeText(this, "Registro fallido: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
                 }
-            } else {
-                // Si el registro falla, muestra un mensaje al usuario.
-                Toast.makeText(this, "Registro fallido: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
             }
-        }
     }
-
 }
-
